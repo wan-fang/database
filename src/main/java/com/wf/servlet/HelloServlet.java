41d1148fd7d0c1c3faec68e9412b53b4997cb6c1
dell <2606189834@qq.com>
2020-11-19 21:50:29 +0800

    init project

---
+++ src/main/java/com/wf/servlet/HelloServlet.java
@@ -0,0 +1,23 @@
+package com.wf.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * dell 2020/11/19 21:49
+ * wanfang
+ */
+@WebServlet(name = "HelloServlet")
+public class HelloServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
